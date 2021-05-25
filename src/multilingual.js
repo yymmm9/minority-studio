fddf264d5234b9a7e38578471480b292110f812d
yymmm9 <46626991+yymmm9@users.noreply.github.com>
2021-05-25 08:47:50 +0200

    Update multilingual.js

--- src/multilingual.js
+++ src/multilingual.js
@@ -1,8 +1,5 @@
 const lang = navigator.language;
 
-console.log(lang);
-
-docum
 if (lang == 'zh-CN'){
           document.getElementById("rta--wechat").classList.add("show");
           document.getElementById("block").classList.add("show");
